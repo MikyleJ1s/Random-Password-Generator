5ece988c02bd98218dd1b1b8642b8e7c84588f23
MikyleJ1s <mikylejones@gmail.com>
2023-03-15 10:36:51 +0200

    bug fixes

--- Implementation/random_password_generator.py
+++ Implementation/random_password_generator.py
@@ -1,37 +1,55 @@
-from flask import Flask, redirect, render_template, request, url_for
-import random, string
-import numpy as np
+# modules used ...
+from flask import Flask, redirect, render_template, request, url_for, Response
+import random, string, logging, time
 
+# logger configurations ...
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename="logs.log", filemode='a', level = logging.DEBUG, format = f"%(asctime)s %(levelname)s %(message)s")
 
 app = Flask(__name__)
 
+# direct user to the main page ...
 @app.route('/')
 def home_page():
+    app.logger.info("Redirected to the main page")
     return redirect(url_for('generate_password'))
 
+# main page logic ...
 @app.route('/generate_password', methods=['GET', 'POST'])
 def generate_password():
-
+    # letters available ....
     ascii_lowercase = string.ascii_lowercase
     ascii_uppercase = string.ascii_uppercase
-
+    # digts available ...
     digits = string.digits
-
+    # punctuation available ...
     punctuation = string.punctuation
     
     if request.method == 'POST':
+        
+        # check the user input ...
+        
+        app.logger.info("Checking the user input")
+        
         use_lowercase = request.form.get("use_lowercase")
         use_uppercase = request.form.get("use_uppercase")
         use_digits = request.form.get("use_digits")
         use_punctuation = request.form.get("use_punctuation")
+                
         password_range = []
         
+        # check if a length was specified ...
         if request.form['use_length']!="":
+            # length was given now check if any of the customization boxes was checked ...
             if not use_lowercase and not use_uppercase and not use_digits and not use_punctuation:
-                # password_range = ascii_lowercase + ascii_uppercase + digits + punctuation
-                return render_template('home_page.html', unique_password = "")
-        
+                # no box has been checked so return an error message ... 
+                app.logger.info("Can't return a password because no customization was provided")
+                return Response(status=204) 
+                #return render_template('home_page.html', unique_password = "") # this refreshes the whole page ...
+
+            # length was given and at least one of the boxes was ticked ...
             else:
+                # check which boxes were chosen ...
                 if use_lowercase:
                     password_range += ascii_lowercase
                 if use_uppercase:
@@ -39,12 +57,19 @@ def generate_password():
                 if use_digits:
                     password_range += digits
                 if use_punctuation:
-                    password_range += punctuation        
-    
+                    password_range += punctuation   
+            
+            # output a password according to the lenght and customization given ...
+            app.logger.info("Returning a unique password")
             return render_template('home_page.html', unique_password = "".join(random.choices(password_range, k=int(request.form['use_length']))))
-        return render_template('home_page.html', unique_password = "")
+        
+        # length was not given so return an error ...
+        app.logger.info("Can't return a password because no length was provided")
+        return Response(status=204) 
+        #return render_template('home_page.html', unique_password = "") # this refreshes the whole page ...
     
     elif request.method == 'GET':
+        app.logger.info("Going to the main page")
         return render_template('home_page.html')
 
 if __name__ == '__main__':
--- Implementation/static/functions.js
+++ Implementation/static/functions.js
@@ -1,11 +1,36 @@
 function button_logic() {
     var use_length = document.forms["user_inputs"]["use_length"].value;
-    if (use_length == "" || use_length == null) { 
-             document.getElementById('generate_button').disabled = true; 
-               return true;
-         } else { 
-             document.getElementById('generate_button').disabled = false;
-             return false;
+    var use_length = document.forms["user_inputs"]["use_length"].value;
+    var use_lowercase = document.forms['user_inputs']["use_lowercase"].checked;
+    var use_uppercase = document.forms['user_inputs']["use_uppercase"].checked;
+    var use_digits = document.forms['user_inputs']["use_digits"].checked;
+    var use_punctuation = document.forms['user_inputs']["use_punctuation"].checked;
+
+
+    if (use_length != "" & (use_lowercase || use_uppercase || use_digits || use_punctuation)) { 
+        document.getElementById('generate_button').disabled = false;
+        return false;
          }  
+    else{ 
+        document.getElementById('generate_button').disabled = true; 
+        return true;
+         } 
 };
 
+function validate() {
+    var x = document.getElementById("snackbar");
+    var use_length = document.forms["user_inputs"]["use_length"].value;
+    var use_lowercase = document.forms['user_inputs']["use_lowercase"].checked;
+    var use_uppercase = document.forms['user_inputs']["use_uppercase"].checked;
+    var use_digits = document.forms['user_inputs']["use_digits"].checked;
+    var use_punctuation = document.forms['user_inputs']["use_punctuation"].checked;
+    if (use_length == "" || use_length == null) { 
+        x.className = "show";}
+
+    else if(!use_lowercase & !use_uppercase & !use_digits & !use_punctuation){
+        x.className = "show";
+    }
+
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
+    return true;
+}
